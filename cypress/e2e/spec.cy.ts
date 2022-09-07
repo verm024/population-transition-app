1b6757d5afa5dfb6672c1ae1a4b206f2801a5806
Muhammad Naufal <45099597+verm024@users.noreply.github.com>
2022-09-07 17:55:38 +0700

    Task / 12 / Create Initial Test Cases (#10)
    * Initial Setup Cypress
    
    * Create Initial Test Cases

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,6 @@
+describe("Population Transition App main test cases", () => {
+  it("Can see prefecture list", () => {});
+  it("Can't view chart when no prefecture is checked", () => {});
+  it("Can select and unselect one of the prefecture checkboxes at a time", () => {});
+  it("Can select and unselect multiple prefectures at a time", () => {});
+});
