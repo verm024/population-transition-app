4bc124a693d1f854b998bc0ec9e2ab1e804bcd72
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-09-07 17:04:31 +0700

    Create Initial Test Cases

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-  });
+describe("Population Transition App main test cases", () => {
+  it("Can see prefecture list", () => {});
+  it("Can't view chart when no prefecture is checked", () => {});
+  it("Can select and unselect one of the prefecture checkboxes at a time", () => {});
+  it("Can select and unselect multiple prefectures at a time", () => {});
 });
